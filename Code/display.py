4a6d11ac964942645356e74c4722ed13125d65a7
srecteamextremist <89315142+srecteamextremist@users.noreply.github.com>
2021-08-24 16:19:36 +0530

    Initial Commit

--- Code/display.py
+++ Code/display.py
@@ -25,7 +25,16 @@ def disp_png_jpg(img,dataset):
     else:
         x.empty()
         st.image(himg,caption="Heatmap Image",use_column_width=True)
-        cols[1].image(pre,caption="Segmented Mask",width=256)        
+        cols[1].image(pre,caption="Segmented Mask",width=256)
+        if dataset=="BRAIN":
+            st.markdown("<h2 style='text-align: left; color: black;'>MODEL SUMMARY: <br><pre> loss: 0.0027 <br> accuracy: 0.9979 <br> mean_io_u: 0.4916 <br> val_loss: 0.0525 <br> val_accuracy: 0.9911 <br> val_mean_io_u: 0.4910 </h2>", 
+                unsafe_allow_html=True)
+        elif dataset=='RETINA':
+            st.markdown("<h2 style='text-align: left; color: black;'>MODEL SUMMARY: <br><pre> loss: 0.4405 <br> accuracy: 0.9489 <br> mean_io_u: 0.4733 <br> val_loss: 0.8757 <br> val_accuracy: 0.9155 <br> val_mean_io_u: 0.4733 </h2>", 
+                unsafe_allow_html=True)
+        else:
+            st.markdown("<h2 style='text-align: left; color: black;'>MODEL SUMMARY: <br><pre> loss: 0.0058 <br> accuracy: 0.9968 <br> mean_io_u: 0.3868 <br> val_loss: 0.0758 <br> val_accuracy: 0.9853 <br> val_mean_io_u: 0.3850</h2>", 
+                unsafe_allow_html=True)
        
         
 def get_binary_file_downloader_html(bin_file, file_label='File'):
