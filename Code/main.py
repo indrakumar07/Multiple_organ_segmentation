78670d05b8a472ac675ce82f7be7fbe5544b618e
srecteamextremist <89315142+srecteamextremist@users.noreply.github.com>
2021-08-24 14:37:52 +0530

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import display
 
-import os
-def file_selector(folder_path='Downloads/'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-filename = file_selector()
-st.write('You selected `%s`' % filename)
 st.image('Images/head.gif')
 dataset=st.selectbox(' Select Data Class:',("LUNG","LIVER","RETINA","BRAIN"))
 
